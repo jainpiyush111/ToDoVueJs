b6fc6404105dab870a55e89d2188e6cf0f33844c
Piyush Jain <jainpiyush111@gmail.com>
2017-10-25 00:13:04 +0530

    Added TO DO title

---
+++ src/components/ToDo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ToDo',
+    data () {
+      return {
+        title: 'TO DO APP'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1 {
+    font-weight: normal;
+  }
+</style>
